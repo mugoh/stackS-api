215a36dcfd28277ddd99a139e3b93e6d9042d5df
root <mugoh.ks@gmail.com>
2018-12-29 16:42:49 +0300

    [Chore #162867683] Clear unused class behaviour

--- app/v1/models/base_model.py
+++ app/v1/models/base_model.py
@@ -11,6 +11,3 @@ class BaseModel:
     def __init__(self, data):
         self.created_at = datetime.datetime.utcnow()
         self.id = len(data) + 1
-
-    def delete(self, data, item):
-        return data.remove(item)
