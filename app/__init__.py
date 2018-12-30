00df4874dc879837029d0ab6318beb6c474fec93
root <mugoh.ks@gmail.com>
2018-12-30 22:10:53 +0300

    [Chore #162889373] Configure jwt for blacklisting of tokens

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.v1.views.users import auth
+from app.v1.views.users import auth, blacklisted_tokens
 from instance.config import APP_CONFIG
 from flask_jwt_extended import JWTManager
 
@@ -7,8 +7,16 @@ from flask_jwt_extended import JWTManager
 def create_app(config_setting):
     app = Flask(__name__)
     app.register_blueprint(auth)
+    app.config['JWT_BLACKLIST_ENABLED'] = True
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+
     jwt = JWTManager(app)
     app.config.from_object(APP_CONFIG[
         config_setting.strip().lower()])
+
+    @jwt.token_in_blacklist_loader
+    def check_blacklisted_token(token):
+        jti = token['jti']
+        return jti in blacklisted_tokens
 
     return app
